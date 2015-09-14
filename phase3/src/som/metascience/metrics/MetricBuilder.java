9a8400219ca16df7283622e86e9e51d696ff3d78
Javier Canovas <me@jlcanovas.es>
2015-09-14 16:08:00 +0200

    Documenting metricBuilder

--- phase3/src/som/metascience/metrics/MetricBuilder.java
+++ phase3/src/som/metascience/metrics/MetricBuilder.java
@@ -3,16 +3,36 @@ package som.metascience.metrics;
 import som.metascience.DBInfo;
 import som.metascience.MetricData;
 
-import java.io.File;
-import java.sql.Connection;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
+/**
+ * This class controls the list of metrics to apply. Furthermore, the class knows the category to print
+ * for each metric and how to build them.
+ *
+ * To add new metrics, follow these steps:
+ * 1. Implement your metric as a subclass of {@link Metric}
+ * 2. Add your class to the "metrics" array (as a class)
+ * 3. Add a new case in "getCategory()" method, which will return the category of the metric
+ *    (in a CSV style if it covers several categories)
+ * 4. Add a new case in "buildMetric()" method to build an instance of your metric
+ *
+ * The set of metrics will be retrieved and executed by {@link som.metascience.MetricCalculator}
+ */
 public class MetricBuilder {
-    MetricData metricData;
+    /**
+     * Basic metric data (used to build metrics)
+     */
+    private MetricData metricData;
+    /**
+     * Database info (used when building metrics)
+     */
     private DBInfo dbInfo;
 
+    /**
+     * The set of metrics to consider
+     * (add here yours if you want to add another one, recall the steps to follows in the description of this class)
+     */
     private static Class[] metrics = { AllPapers.class, AllAuthors.class, AverageDegree.class };
 
     public MetricBuilder(MetricData metricData, DBInfo dbInfo) {
@@ -20,6 +40,10 @@ public class MetricBuilder {
         this.dbInfo = dbInfo;
     }
 
+    /**
+     * Returns the list of metrics to be calculated
+     * @return A lisk of {@link Metric}s
+     */
     public static List<Class> getMetrics() {
         return Arrays.asList(metrics);
     }
@@ -27,6 +51,7 @@ public class MetricBuilder {
     /**
      * Returns the string to show as category for this metric.
      * If the metric returns several values, the category will be comma-separated value
+     * (add here yours if you want to add another one, recall the steps to follows in the description of this class)
      *
      * @param m The metrics from which to get the category
      * @return String with the name/s of the categories to show
@@ -39,10 +64,18 @@ public class MetricBuilder {
             result = "Num_authors";
         } else if (m == AverageDegree.class) {
             result = "AvgDegree_allEditions,AvgDegree_1_LastEdition,AvgDegree_2_LastEdition,AvgDegree_3_LastEdition,AvgDegree_4_LastEdition,AvgDegree_5_LastEdition";
-        }
+        } else
+            throw new IllegalArgumentException("There is no category for such a metric");
         return result;
     }
 
+    /**
+     * Builds a specific metric
+     * (add here yours if you want to add another one, recall the steps to follows in the description of this class)
+     *
+     * @param m The Metric to be build
+     * @return The metric built
+     */
     public Metric buildMetric(Class m) {
         Metric result = null;
         if (m == AllPapers.class) {
@@ -51,7 +84,8 @@ public class MetricBuilder {
             result = new AllAuthors(metricData, dbInfo);
         } else if (m == AverageDegree.class) {
             result = new AverageDegree(metricData);
-        }
+        }  else
+            throw new IllegalArgumentException("There is no builder for such a metric");
         return result;
     }
 
