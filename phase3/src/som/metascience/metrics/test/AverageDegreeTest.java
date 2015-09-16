aeb8f02a422147f510b23297fed204ec90dd1db7
Javier Canovas <me@jlcanovas.es>
2015-09-16 11:13:07 +0200

    Adding new test case for models

--- phase3/src/som/metascience/metrics/test/AverageDegreeTest.java
+++ phase3/src/som/metascience/metrics/test/AverageDegreeTest.java
@@ -10,16 +10,28 @@ import static org.junit.Assert.*;
 
 public class AverageDegreeTest extends MetricTest {
     public static final File GRAPHS = new File("testData/graphs");
-    public static final File TEST_FILE = new File("testData/importData/OCL-Models.properties");
+    public static final File TEST_FILE_1 = new File("testData/importData/OCL-Models.properties");
+    public static final File TEST_FILE_2 = new File("testData/importData/International-Conference-on-Model-Driven-Engineering-Languages-and-Systems.properties");
 
     @Test
-    public void testCalculateAverageDegree() throws Exception {
-        MetricData metricData = buildMetricData(GRAPHS, TEST_FILE);
+    public void testCalculateAverageDegree1() throws Exception {
+        MetricData metricData = buildMetricData(GRAPHS, TEST_FILE_1);
         AverageDegree ad = new AverageDegree(metricData);
 
         Double result = Double.valueOf(ad.calculateAverageDegree(metricData.getFullGraph()));
         Double roundedResult = roundTo2Decimals(result);
 
         assertEquals(3.769, roundedResult.doubleValue(), 0.0);
+    }
+
+    @Test
+    public void testCalculateAverageDegree2() throws Exception {
+        MetricData metricData = buildMetricData(GRAPHS, TEST_FILE_2);
+        AverageDegree ad = new AverageDegree(metricData);
+
+        Double result = Double.valueOf(ad.calculateAverageDegree(metricData.getFullGraph()));
+        Double roundedResult = roundTo2Decimals(result);
+
+        assertEquals(4.024, roundedResult.doubleValue(), 0.0);
     }
 }
--- phase3/src/som/metascience/metrics/test/ConnectedComponentsTest.java
+++ phase3/src/som/metascience/metrics/test/ConnectedComponentsTest.java
@@ -10,14 +10,24 @@ import static org.junit.Assert.*;
 
 public class ConnectedComponentsTest extends MetricTest {
     public static final File GRAPHS = new File("testData/graphs");
-    public static final File TEST_FILE = new File("testData/importData/OCL-Models.properties");
+    public static final File TEST_FILE_1 = new File("testData/importData/OCL-Models.properties");
+    public static final File TEST_FILE_2 = new File("testData/importData/International-Conference-on-Model-Driven-Engineering-Languages-and-Systems.properties");
 
     @Test
-    public void testCalculateGraphComponents() throws Exception {
-        MetricData metricData = buildMetricData(GRAPHS, TEST_FILE);
+    public void testCalculateGraphComponents1() throws Exception {
+        MetricData metricData = buildMetricData(GRAPHS, TEST_FILE_1);
         ConnectedComponents cc = new ConnectedComponents(metricData);
 
         Integer result = Integer.valueOf(cc.calculateGraphComponents(metricData.getFullGraph()));
         assertEquals(19, result.intValue());
+    }
+
+    @Test
+    public void testCalculateGraphComponents2() throws Exception {
+        MetricData metricData = buildMetricData(GRAPHS, TEST_FILE_2);
+        ConnectedComponents cc = new ConnectedComponents(metricData);
+
+        Integer result = Integer.valueOf(cc.calculateGraphComponents(metricData.getFullGraph()));
+        assertEquals(102, result.intValue());
     }
 }
